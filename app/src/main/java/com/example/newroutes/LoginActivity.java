c7261fde4b5cc21c1c9f5e659240ac9a8f84a546
Jackson Keel-Atkins <jkatkins@fb.com>
2020-07-13 13:43:32 -0400

    added project code to get, parse deployed, login screen created

---
+++ app/src/main/java/com/example/newroutes/LoginActivity.java
@@ -0,0 +1,73 @@
+package com.example.newroutes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.newroutes.databinding.ActivityLoginBinding;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText etUsername;
+    EditText etPassword;
+    Button btnLogin;
+    ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setting up ViewBinding
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //Check if user already logged in
+        if (ParseUser.getCurrentUser() != null) {
+            goMainActivity();
+        }
+
+        //Defining variables from XML file
+        etUsername = binding.etUsername;
+        etPassword = binding.etPassword;
+        btnLogin = binding.btnLogin;
+
+        //On Click Listeners
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                loginUser(username,password);
+            }
+        });
+    }
+
+    private void loginUser(String username, String password) {
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    Toast.makeText(LoginActivity.this, "Error logging in", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                goMainActivity();
+
+            }
+
+        });
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(this,MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
